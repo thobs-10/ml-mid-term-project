2acef033fce6d38ffd9baf91cc378d40280232ad
thobela <thobela.sixpence@gmail.com>
2024-11-17 21:52:27 +0200

    removed the split abstractmeethod as i will not be using it everywhere

--- src/components/component.py
+++ src/components/component.py
@@ -1,15 +1,13 @@
 from abc import ABC, abstractmethod
+import pandas as pd
 
 class Component(ABC):
+
     """Abstract base class for components."""
     @abstractmethod
-    def load_data(self) -> None:
+    def load_data(self, filepath: str) -> pd.DataFrame:
         pass
 
     @abstractmethod
-    def split_data(self) -> None:
-        pass
-
-    @abstractmethod
-    def save_data(self) -> None:
+    def save_data(self, df: pd.DataFrame, filepath: str) -> None:
         pass
