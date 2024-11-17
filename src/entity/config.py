a27a54931f3d4e6092c4dfb3f2b40f1a67cfb66f
thobela <thobela.sixpence@gmail.com>
2024-11-17 21:54:55 +0200

    updated the config paths to use dotenv file

--- src/entity/config.py
+++ src/entity/config.py
@@ -1,29 +1,32 @@
+import os
 from dataclasses import dataclass, field
-from pydantic import BaseModel
+from dotenv import load_dotenv
+
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+load_dotenv(dotenv_path=os.path.join(root_dir, '.env'))
 
 
 @dataclass
-class DataIngestion(BaseModel):
-    raw_data_path : str
+class DataIngestion:
+    raw_data_path : str = os.getenv('RAW_PATH_FILE')
 
 @dataclass
 class DataProcessing:
-    processed_data_path: str 
+    processed_data_path: str = os.getenv('PROCESSED_PATH_FILE')
 
 @dataclass
-class FeatureEngineering:
-    data_processing: DataProcessing = field(default_factory=DataProcessing)
-    engineered_data_path: str
+class ArtifactStore:
+    artifacts_path: str = os.getenv('ARTIFACTS_PATH')
+@dataclass
+class FeatureEngineeringConfig:
+    feature_engineered_data_path: str =os.getenv('FEATURE_ENGINEERED_DATA_PATH')
     
-    def combine_full_path(self):
-        self.engineered_data_path = self.data_processing.processed_data_path
-        return self.engineered_data_path
-
 @dataclass
 class ModelTraining:
     model_data_path : str
     model_output_path : str
-    feature_engineered_data : FeatureEngineering = field(default_factory=FeatureEngineering)
+    feature_engineered_data : FeatureEngineeringConfig = field(default_factory=FeatureEngineeringConfig)
     
     def combine_data_fullpath(self):
         self.model_data_path = self.feature_engineered_data.combine_full_path()
