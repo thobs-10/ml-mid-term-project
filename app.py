06b7b8fb4d7b02baacb8a0cf912700fbb2645f18
thobela <thobela.sixpence@gmail.com>
2024-11-24 11:01:00 +0200

    made small changes while debuggging running the app on the docker container

--- app.py
+++ app.py
@@ -1,76 +1,63 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import List, Optional, Union, Dict, Literal, Any
+from dotenv import load_dotenv
 import joblib
+import os
 import uvicorn
 from src.exception import AppException
+import pandas as pd
+import numpy as np
+
+
+load_dotenv()
 
 app = FastAPI()
 
-class PredictionRequest(BaseModel):
-    temperature : float
-    hour: int
-    solar_radication : float
-    seasons: str
-    dew_point_temperature : float
-    functioning_day: bool
-    rainfall: float
+model_path = 'artifacts/staged_model'
 
-    def __post__init__(self):
-        if self.seasons not in ['Winter', 'Spring', 'Summer', 'Fall']:
-            raise HTTPException(status_code=400, detail="Invalid seasons. Expected 'Winter', 'Spring', 'Summer', or 'Fall'")
-        if not (0 <= self.hour <= 23):
-            raise HTTPException(status_code=400, detail="Invalid hour. Expected a value between 0 and 23")
-        if not (0 <= self.solar_radiation <= 10):
-            raise HTTPException(status_code=400, detail="Invalid solar radiation. Expected a value between 0 and 10")
-        if not (0 <= self.dew_point_temperature <= 30):
-            raise HTTPException(status_code=400, detail="Invalid dew point temperature. Expected a value between 0 and 30")
-        if not (0 <= self.rainfall <= 100):
-            raise HTTPException(status_code=400, detail="Invalid rainfall. Expected a value between 0 and 100")
-        if self.functioning_day not in [True, False]:
-            raise HTTPException(status_code=400, detail="Invalid functioning day. Expected a boolean value")
-
-
-class PredictionResponse(BaseModel):
-    prediction: float
-    message : Union[Dict[dict, str], Any]
-
-class StatusResponse(BaseModel):
-    message: str
-    status : Dict[dict, str]
+async def preprocess_input(model_input: dict)-> pd.DataFrame:
+    df = pd.DataFrame(model_input)
+    df['seasons'] = df['seasons'].map({'Spring': 1, 'Summer': 2, 'Autumn': 3, 'Winter': 4})
+    df['functioning_day'] = np.where(df['functioning_day'] =='Yes', 1, 0)
+    return df
 
 async def load_model_pipeline():
-    model_path = ''
+    full_path = os.path.join(model_path, 'model_pipeline.joblib')
+    if not os.path.exists(full_path):
+            raise FileNotFoundError(f"Model file not found at {full_path}")
     try:
-        model = joblib.load(model_path)
+        model = joblib.load(full_path)
         return model
     except FileNotFoundError as e:
         raise AppException("Failed to load model", e)
 
-async def get_request(request: PredictionRequest)-> dict:
+async def get_request(request: dict)-> dict:
     mode_input_dict= {
-    'temperature' : request.temperature,
-    'hour' : request.hour,
-    'solar_radiation' : request.solar_radiation,
-    'seasons' : request.seasons,
-    'dew_point_temperature' : request.dew_point_temperature,
-    'functioning_day' : request.functioning_day,
-    'rainfall' : request.rainfall
+    'temperature' : [request['temperature']],
+    'hour' : [request['hour']],
+    'solar_radiation' : [request['solar_radiation']],
+    'seasons' : [request['seasons']],
+    'dew_point_temperature' : [request['dew_point_temperature']],
+    'functioning_day' : [request['functioning_day']],
+    'rainfall' : [request['rainfall']]
     }
     return mode_input_dict
 
-@app.get('/', response_model=PredictionResponse, status_code= status.HTTP_200_OK)
+@app.get('/', status_code= status.HTTP_200_OK)
 async def health():
-    return {"message": {"status": "OK, welcome to the bike sharing API ML project"}}
+    return {"message ": "welcome to the bike sharing API ML project"}
 
-@app.post("/predict", response_model=PredictionResponse, status_code= status.HTTP_202_ACCEPTED)
-async def predict(request: PredictionRequest):
+@app.post("/predict", status_code= status.HTTP_202_ACCEPTED)
+async def predict(request: dict):
    
-    model_request = get_request(request)
+    model_request = await get_request(request)
+    input = await preprocess_input(model_request)
     model = await load_model_pipeline()
-    prediction = model.predict(**model_request)
+    prediction = model.predict(input)
 
-    return { prediction: {"message": "Number of bikes needed"}}
+    return {"message": "Number of bikes needed",
+            "data": prediction[0]}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True,)
